1672711cc3a3d1718316437df66c29c5272650cf
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-07-11 12:01:29 +0200

    New try at implementing WiFi Direct into the test app. Followed API until creating new BroadcastReceiver class. Added useful variables in MainActivity.kt and created new template for next class.

--- wifi_dir_test/app/src/main/java/com/example/wifi_dir_test/MainActivity.kt
+++ wifi_dir_test/app/src/main/java/com/example/wifi_dir_test/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.wifi_dir_test
 
 import android.content.Context
-import android.content.IntentFilter
-import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -19,16 +17,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var typeMessage: EditText
     private lateinit var sendMessage: Button
 
-    // WiFi Direct
-    private lateinit var channel: WifiP2pManager.Channel
-    private lateinit var manager: WifiP2pManager
-
+    // event filter
     private val intentFilter = IntentFilter()
+
+    // WifiP2PManager
+    private lateinit var manager: WifiP2pManager
+    private lateinit var channel: WifiP2pManager.Channel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // init GUI
+        initGUI()
+        // init intentFilter
+        initIntentFilter()
+        // init WifiP2PManager
+        initWifiP2PManager()
+    }
+
+    /**
+     * Initialize GUI related variables.
+     */
+    private fun initGUI() {
         // link to GUI
         textView = findViewById(R.id.textView)
         typeMessage = findViewById(R.id.typeMessage)
@@ -36,7 +47,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         // set Button
         sendMessage.setOnClickListener(this)
+    }
 
+    /**
+     * Initialize IntentFilter related possible actions.
+     */
+    private fun initIntentFilter() {
         // Indicates a change in the Wi-Fi P2P status.
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
         // Indicates a change in the list of available peers.
@@ -45,7 +61,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
         // Indicates this device's details have changed.
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+    }
 
+    /**
+     * Initialize WifiP2PManager instance and get a Channel object to connect
+     * the app to the Wi-Fi P2P framework.
+     */
+    private fun initWifiP2PManager() {
         manager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
         channel = manager.initialize(this, mainLooper, null)
     }
