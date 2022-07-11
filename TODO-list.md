0bad6bf6616e9d750727f28632a3b378b7bab492
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-07-11 14:17:09 +0200

    added a todo list file for the project

---
+++ TODO-list.md
@@ -0,0 +1,6 @@
+# TODO List
+
+## Test Project
+
+[] Create DeviceListFragment class for saving al possible devices in a list 
+
---
+++ wifi_dir_test/app/src/main/java/com/example/wifi_dir_test/WiFiDirectBroadcastReceiver.kt
@@ -0,0 +1,48 @@
+package com.example.wifi_dir_test
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pManager
+
+class WiFiDirectBroadcastReceiver(manager: WifiP2pManager,
+                                  channel: WifiP2pManager.Channel,
+                                  activity: MainActivity) : BroadcastReceiver() {
+
+    private val manager: WifiP2pManager = manager
+    private val channel: WifiP2pManager.Channel = channel
+    private val activity: MainActivity = activity
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when(intent.action) {
+            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                // Determine if Wifi P2P mode is enabled or not, alert
+                // the Activity.
+                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                activity.isWifiP2pEnabled = state == WifiP2pManager.WIFI_P2P_STATE_ENABLED
+            }
+            WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+
+                // The peer list has changed! We should probably do something about
+                // that.
+
+            }
+            WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+
+                // Connection state changed! We should probably do something about
+                // that.
+
+            }
+            WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+                (activity.supportFragmentManager.findFragmentById(R.id.frag_list) as DeviceListFragment)
+                    .apply {
+                        updateThisDevice(
+                            intent.getParcelableExtra(
+                                WifiP2pManager.EXTRA_WIFI_P2P_DEVICE)!! as WifiP2pDevice
+                        )
+                    }
+            }
+        }
+    }
+}
